3284e8e2fed9cbdadd4da29a52991fe8f354596a
HarunaTech <hamodharuna@gmail.com>
2024-03-13 00:01:53 +0200

    Added vite App boiler plate and basics components

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+
+import "../App.css"
+
+function Header() {
+
+  return (
+    <>
+      <div>
+       <header>
+        This is header
+       </header>
+      </div>
+     
+    </>
+  )
+}
+
+export default Header
